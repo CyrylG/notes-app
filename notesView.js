bfbc62edd5860cb51c26b426131752cf61f123fa
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-05-31 15:50:23 +0100

    handling forms input

--- notesView.js
+++ notesView.js
@@ -3,6 +3,12 @@ class NotesView {
     constructor(model) {
         this.model = model;
         this.mainContainerEl = document.querySelector('#main-container');
+        this.button = document.querySelector('#add-note');
+        this.input = document.querySelector('#user-input');
+        this.button.addEventListener('click', () => {
+            this.model.addNote(this.input.value);
+            this.displayNotes();
+        })
     };
 
     displayNotes() {
--- notesView.test.js
+++ notesView.test.js
@@ -21,4 +21,19 @@ describe('NotesView', () => {
 
         expect(document.querySelectorAll('div.note').length).toEqual(2);
     });
+
+    it('displays 2 notes', () => {
+        document.body.innerHTML = fs.readFileSync('./index.html');
+
+        const model = new NotesModel();
+        const view = new NotesView(model);
+        const input = document.querySelector('#user-input');
+        input.value = 'New note';
+
+        const button = document.querySelector('#add-note');
+        button.click();
+
+        expect(document.querySelectorAll('div.note').length).toEqual(1);
+        expect(document.querySelectorAll('div.note')[0].textContent).toEqual('New note');
+    });
 });
